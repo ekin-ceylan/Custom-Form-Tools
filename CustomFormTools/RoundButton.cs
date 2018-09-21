682f8ec1cf9c4674d3b47fd9b523004ef53830d3
ekin-ceylan <43453604+ekin-ceylan@users.noreply.github.com>
2018-09-21 21:36:57 +0300

    adding to git

---
+++ CustomFormTools/RoundButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+namespace CustomFormTools
+{
+    public class RoundButton : Button
+    {
+        readonly Pen _pen = new Pen(Color.Red);
+
+        public int BorderWidth
+        {
+            get { return (int)_pen.Width; }
+            set
+            {
+                _pen.Width = value;
+                Invalidate();                
+            }
+        }
+
+        public Color BorderColor
+        {
+            get { return _pen.Color; }
+            set
+            {
+                _pen.Color = value;
+                Invalidate();
+            }
+        }
+
+        protected override void InitLayout()
+        {
+            Paint += Pintar;
+            base.InitLayout();
+        }
+
+        private void Pintar(object sender, PaintEventArgs e)
+        {
+            GraphicsPath buttonPath  = new GraphicsPath();
+            Rectangle rectangle = ClientRectangle;
+
+            rectangle.Inflate(-2, -2);
+            _pen.DashStyle = DashStyle.Solid;
+
+            e.Graphics.DrawEllipse(_pen, rectangle);
+            //rectangle.Inflate(0, 0);
+            buttonPath.AddEllipse(rectangle);
+            Region = new Region(buttonPath);
+
+        }
+    }
+}
---
+++ CustomFormTools/TransparentRichTextBox.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomFormTools
+{
+    public class TransparentRichTextBox : RichTextBox
+    {
+        public TransparentRichTextBox()
+        {
+            SetStyle(ControlStyles.Opaque, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, false);
+            //TextChanged += TransparentLabel_TextChanged;
+            VScroll += TransparentLabel_TextChanged;
+        }
+
+        void TransparentLabel_TextChanged(object sender, System.EventArgs e)
+        {
+            ForceRefresh();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams parms = base.CreateParams;
+                parms.ExStyle |= 0x20;  // Turn on WS_EX_TRANSPARENT
+                return parms;
+            }
+        }
+        public void ForceRefresh()
+        {
+            UpdateStyles();
+        }
+    }
+}
